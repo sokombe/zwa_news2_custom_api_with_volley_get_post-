2ac8e4feb5e95e547c0e7f31c91aa1512403a11f
Minasquin sokombe <66481390+sokombe@users.noreply.github.com>
2022-06-17 18:40:05 +0100

    add prof
    add prof

--- app/src/main/java/com/example/zwanews/MainActivity.java
+++ app/src/main/java/com/example/zwanews/MainActivity.java
@@ -119,8 +119,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-       System.out.println("#######################################################");
-        System.out.println(auth.getCurrentUser().getPhotoUrl().toString());
     }
 
 
--- app/src/main/java/com/example/zwanews/Models/Comments.java
+++ app/src/main/java/com/example/zwanews/Models/Comments.java
@@ -63,3 +63,4 @@ public class Comments  implements  Serializable{
     }
 
 }
+
